d4ecf711ac85def75a47bb58d00d9e40b5ab2270
Lucas Barbosa <lucasfrancadev3@gmail.com>
2026-06-20 20:54:30 -0300

    testando o envio do sms

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 import os
 from supabase import create_client
 
+
+
 resultado = load_dotenv()
 
 print("Arquivo carregado?", resultado)
@@ -12,9 +14,22 @@ print("Diretório atual:", os.getcwd())
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
 
+instance_id = os.getenv("ZAPI_INSTANCE_ID")
+token = os.getenv("ZAPI_TOKEN")
+
+print(instance_id, token)
 supabase = create_client(url, key)
 resposta = supabase.table("contatos").select("*").execute()
 
+contatos = resposta.data
+
+for contato in contatos:
+    nome = contato["nome"]
+    numero = contato["telefone"]
+
+    mensagem = (f"Ola {nome}, tudo bem com você?")
+    
+    print(mensagem)
 
 print(resposta.data)
 print("Conexao realizada com sucesso")
